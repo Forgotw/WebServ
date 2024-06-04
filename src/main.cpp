3ed747ffebc6e7d0c38b1eb65983863b6f1f8d6c
R4yL <luca.ray@protonmail.ch>
2024-06-04 16:11:52 +0200

    clean comment

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:27:33 by lsohler           #+#    #+#             */
-/*   Updated: 2024/05/22 15:02:22 by lray             ###   ########.fr       */
+/*   Updated: 2024/06/04 15:38:06 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,41 +33,3 @@ int main(int ac, char **av) {
 	}
 	return 0;
 }
-// #include <unistd.h>
-// #include <fstream>
-// #include <string>
-
-// bool fuckingCanOpenFile(const std::string& filename) {
-//     char cwd[1024];
-//     if (getcwd(cwd, sizeof(cwd)) == NULL) {
-//         std::cerr << "Erreur lors de la récupération du répertoire de travail actuel." << std::endl;
-//         return false;
-//     }
-
-//     std::string fullPath = std::string(cwd) + "/" + filename;
-//     std::ifstream file(fullPath.c_str());
-// 	std::cout << "Trying to open: " << fullPath.c_str() << std::endl;
-
-//     if (!file.is_open()) {
-//         std::cerr << "Erreur : Impossible d'ouvrir le fichier '" << fullPath << "'." << std::endl;
-//         if (file.fail()) {
-//             std::cerr << "Echec lors de l'ouverture du fichier." << std::endl;
-//         }
-//         return false;
-//     }
-
-//     // Lecture simple pour vérifier le contenu du fichier
-//     std::string line;
-//     while (getline(file, line)) {
-//         std::cout << line << std::endl;
-//     }
-
-//     file.clear();  // Réinitialiser l'état d'erreur
-//     file.close();
-
-//     return true;
-// }
-
-// int main() {
-// 	fuckingCanOpenFile("www/test/test.html");
-// }
